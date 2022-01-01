44bf33016526b24f107a78e59b585b5c86342260
Noraluk <noraluk.kn@gmail.com>
2022-01-02 01:07:48 +0700

    :sparkles: init page segment

---
+++ lib/widgets/page_segment.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:plant_system_mobile/widgets/segment.dart';
+
+class PageSegment extends StatefulWidget {
+  const PageSegment({
+    Key? key,
+    required this.segmentIcons,
+    required this.pages,
+  }) : super(key: key);
+
+  final List<IconData> segmentIcons;
+  final List<Widget> pages;
+
+  @override
+  _PageSegmentState createState() => _PageSegmentState();
+}
+
+class _PageSegmentState extends State<PageSegment> {
+  final PageController _pageController = PageController();
+
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Segment(
+          icons: widget.segmentIcons,
+          pageController: _pageController,
+          pageSelectedIndex: selectedIndex,
+        ),
+        Expanded(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (int index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            children: widget.pages,
+          ),
+        ),
+      ],
+    );
+  }
+}
