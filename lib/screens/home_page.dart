d75f555958d02e461778103eb38e5808cac8386f
Noraluk <noraluk.kn@gmail.com>
2022-01-02 01:09:44 +0700

    :sparkles: move pump switch to anoter file

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plant_system_mobile/blocs/pump/pump_bloc.dart';
-import 'package:plant_system_mobile/models/pump/pump_activated_request_model/pump_activated_request_model.dart';
 import 'package:plant_system_mobile/widgets/loading.dart';
+import 'package:plant_system_mobile/widgets/pump/pump_switch.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,48 +25,25 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<PumpBloc, PumpState>(
-        builder: (context, state) {
-          if (state is PumpLoadingState) {
-            return const Loading();
-          } else if (state is PumpLoadedState) {
-            return _PumpSwitch(isActive: state.isActive!);
-          }
-          return Container();
-        },
-        listener: (context, state) {
-          if (state is PumpErrorState) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('pump invalid'),
-            ));
-            context.read<PumpBloc>().add(PumpLoadedEvent(id: 1));
-          }
-        },
-      ),
-    );
-  }
-}
-
-class _PumpSwitch extends StatelessWidget {
-  const _PumpSwitch({Key? key, required this.isActive}) : super(key: key);
-
-  final bool isActive;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Switch.adaptive(
-        value: isActive,
-        onChanged: (isActive) {
-          context.read<PumpBloc>().add(
-                PumpActivatedEvent(
-                  id: 1,
-                  pumpActivatedRequestModel: PumpActivatedRequestModel(
-                    isActive: isActive,
-                  ),
-                ),
-              );
-        },
+      body: SafeArea(
+        child: BlocConsumer<PumpBloc, PumpState>(
+          builder: (context, state) {
+            if (state is PumpLoadingState) {
+              return const Loading();
+            } else if (state is PumpLoadedState) {
+              return PumpSwitch(isActive: state.isActive!);
+            }
+            return Container();
+          },
+          listener: (context, state) {
+            if (state is PumpErrorState) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('pump invalid'),
+              ));
+              context.read<PumpBloc>().add(PumpLoadedEvent(id: 1));
+            }
+          },
+        ),
       ),
     );
   }
---
+++ lib/widgets/pump/pump_switch.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plant_system_mobile/blocs/pump/pump_bloc.dart';
+import 'package:plant_system_mobile/models/pump/pump_activated_request_model/pump_activated_request_model.dart';
+import 'package:plant_system_mobile/widgets/countdown.dart';
+import 'package:plant_system_mobile/widgets/page_segment.dart';
+
+class PumpSwitch extends StatefulWidget {
+  const PumpSwitch({Key? key, required this.isActive}) : super(key: key);
+
+  final bool isActive;
+
+  @override
+  State<PumpSwitch> createState() => _PumpSwitchState();
+}
+
+class _PumpSwitchState extends State<PumpSwitch> {
+  bool isCountdownFinish = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: PageSegment(
+        segmentIcons: const [
+          Icons.remove,
+          Icons.add,
+        ],
+        pages: [
+          Switch.adaptive(
+            value: widget.isActive,
+            onChanged: (isActive) => _changePumpStatus(
+              context,
+              isActive: isActive,
+            ),
+          ),
+          Countdown(
+            begin: 5,
+            onStart: () {
+              _changePumpStatus(context, isActive: true);
+              setState(() {
+                isCountdownFinish = false;
+              });
+            },
+            onEnd: !isCountdownFinish
+                ? () {
+                    _changePumpStatus(context, isActive: false);
+                    setState(() {
+                      isCountdownFinish = true;
+                    });
+                  }
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _changePumpStatus(
+    BuildContext context, {
+    required bool isActive,
+  }) {
+    context.read<PumpBloc>().add(
+          PumpActivatedEvent(
+            id: 1,
+            pumpActivatedRequestModel: PumpActivatedRequestModel(
+              isActive: isActive,
+            ),
+          ),
+        );
+  }
+}
